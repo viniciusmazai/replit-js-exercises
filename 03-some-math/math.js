ccc215cde65261ad707614667d0a75210d7187ad
Vinicius Cesar <vinicius@cesar.etc.br>
2024-08-27 10:00:38 -0300

    initial commit

---
+++ 03-some-math/math.js
@@ -0,0 +1,14 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   - Replace the strings to the right of the = with the math expression they describe.
+ *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
+ */
+
+const a = "one plus eight"
+const b = "22 times three"
+const c = "the *remainder* of 5/4"
+const d = "the variable 'a' minus 17"
+const e = "the sum of the previous four variables"
+
+module.exports = {a, b, c, d, e}
