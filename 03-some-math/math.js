d92ef4e5f5ebf4807dc28df7503fe19f060e47e9
Vinicius Cesar <vinicius@cesar.etc.br>
2024-08-27 10:03:43 -0300

    replace string expressions with corresponding math operations and fix remainder calculation

--- 03-some-math/math.js
+++ 03-some-math/math.js
@@ -5,10 +5,10 @@
  *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
  */
 
-const a = "one plus eight"
-const b = "22 times three"
-const c = "the *remainder* of 5/4"
-const d = "the variable 'a' minus 17"
-const e = "the sum of the previous four variables"
+const a = 1 + 8
+const b = 22 * 3
+const c = 5 % 4
+const d = a - 17
+const e = a + b + c + d
 
 module.exports = {a, b, c, d, e}
